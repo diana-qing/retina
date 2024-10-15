ab6e85597bdce9bef56430ed8de735c570a8e774
Diana Qing <dianawqing@gmail.com>
2024-10-15 20:17:30 +0000

    add stat on num remaining gaps

--- examples/tcp_stats/src/main.rs
+++ examples/tcp_stats/src/main.rs
@@ -23,19 +23,26 @@ fn main() -> Result<()> {
 
     let num_gaps = AtomicUsize::new(0);
     let num_conn = AtomicUsize::new(0);
+    let num_remaining = AtomicUsize::new(0);
     let callback = |conn: Connection| {
+        num_conn.fetch_add(1, Ordering::Relaxed);
+
         let num_gaps_orig_resp = conn.orig.gaps.len() + conn.resp.gaps.len();
         num_gaps.fetch_add(num_gaps_orig_resp, Ordering::Relaxed);
-        num_conn.fetch_add(1, Ordering::Relaxed);
+        
+        let num_remaining_gaps = conn.orig.content_gaps() + conn.resp.content_gaps();
+        num_remaining.fetch_add(num_remaining_gaps as usize, Ordering::Relaxed);
     };
 
     let mut runtime = Runtime::new(config, filter, callback)?;
     runtime.run();
 
-    let numerator = num_gaps.load(Ordering::SeqCst);
-    let denom = num_conn.load(Ordering::SeqCst);
-    println!("Total number of TCP connections: {:?}", num_conn);
-    println!("Number of gaps across all TCP connections: {:?}", num_gaps);
-    println!("Percent of TCP connections with gaps: {:.2}", (numerator as f64 / denom as f64) * 100.0);
+    let load_num_gaps = num_gaps.load(Ordering::SeqCst);
+    let load_num_conn = num_conn.load(Ordering::SeqCst);
+    println!("Number of TCP connections: {:?}", num_conn);
+    println!("Number of observed gaps across all TCP connections: {:?}", num_gaps);
+    println!("Percent of TCP connections with gaps: {:.2}", (load_num_gaps as f64 / load_num_conn as f64) * 100.0);
+
+    println!("Number of remaining gaps at the end of all TCP connections: {:?}", num_remaining);
     Ok(())
 }
